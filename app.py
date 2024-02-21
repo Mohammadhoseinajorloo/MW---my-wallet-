eae52fbc34b738615c182c96cad0e72a57a20b03
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-02-21 17:18:17 +0330

    Fech:add rejester into app This fech can rejester nito app by username password and mail and this informatin document in user.txt file writer:mohammad-Inspiron-N5010

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from user import User
 from arg_parser import args
 
+def rejester():
+    db = open("user.txt", "r")
+    username = args.Rejester[0]
+    password = args.Rejester[1]
+    mail = args.Rejester[2]
+    user = User(username, password, mail)
+    db = open("user.txt", "a")
+    db.write(username + "," + password + "," +  mail + "\n")
+    print("Success!")
+
+def login():
+    pass
 
 if __name__ == "__main__":
-    mohammad = User(args.login[0], args.login[1], args.login[2])
-    print(mohammad.info())
+    rejester()
--- arg_parser.py
+++ arg_parser.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument("--login",
+parser.add_argument("--Rejester",
                     nargs=3,
                     help="This create a user")
 
